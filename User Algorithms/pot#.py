6a90eecea977c93ba66a40271566f1ef9e3d8ccd
EdwardStables <edward.stables1198@gmail.com>
2018-09-01 17:53:45 +0100

    removed some extra files

--- User Algorithms/pot#.py
+++
@@ -1,9 +0,0 @@
-#A place to get started
-#Use pl.getData([dataset]) to import the dataset called [dataset]. This currently returns the whole dataset as a pandas dataframe
-#Use pl.saveData([dataset]) to save the dataset called [dataset]. This currently  requires [dataset] to be a pandas dataframe.
-#If the dataset has a new name it will be stored in the 'User Datasets' directory in the platform directory.
-
-import platfrom as pl
-import pandas as pd
-
-#Write something fun!

c07ed3a9f26bf4b69218070a56c3dce8df51f64c
EdwardStables <edward.stables1198@gmail.com>
2018-09-06 18:45:41 +0100

    Added controls to algorithms (save, run, open, etc.), added interactions between datasets and algorithms, added basic running of algorithms

--- User_Algorithms/textAlgo.py
+++
@@ -1,8 +0,0 @@
-import platformHelper as pH
-
-
-test1 = pH.getData("test 1")
-test = pH.getData("test")
-pH.setData("new test 1", test1)
-pH.setData("does it fucking work yet", test)
-print(pH.getDataList())
